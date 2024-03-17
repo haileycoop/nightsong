faf99734383583f21196df43fd40b783ed84cc24
Hailey Cooperrider <haileycooperrider@gmail.com>
2024-03-17 13:28:20 +1100

    Quartz sync: Mar 17, 2024, 1:28 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Nightsong",
+    pageTitle: "Nightsong Player Portal",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
